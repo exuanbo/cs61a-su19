48211fd1286042d982b0288a29612d1f687f929f
exuanbo <exuanbo@protonmail.com>
2021-03-02 22:11:34 +0000

    done with lab03

---
+++ labs/lab03/lab03.py
@@ -0,0 +1,76 @@
+""" Lab 3: Recursion and Python Lists """
+
+
+def skip_add(n):
+    """Takes a number x and returns x + x-2 + x-4 + x-6 + ... + 0.
+
+    >>> skip_add(5)  # 5 + 3 + 1 + 0
+    9
+    >>> skip_add(10) # 10 + 8 + 6 + 4 + 2 + 0
+    30
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check('lab03.py', 'skip_add',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 0:
+        return 0
+    return n + skip_add(n - 2)
+
+
+this_file = __file__
+
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(this_file, 'hailstone',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return 1 + hailstone(n // 2)
+    return 1 + hailstone(3 * n + 1)
+
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(this_file, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(1)
+    return summation(n - 1, term) + term(n)
---
+++ labs/lab03/lab03_extra.py
@@ -0,0 +1,66 @@
+""" Optional problems for Lab 3 """
+
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def is_multiple_of(i):
+        if i ** 2 < n:
+            if n % i == 0:
+                return False
+            return is_multiple_of(i + 1)
+        return True
+
+    return is_multiple_of(2)
+
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return 0
+    return ten_pairs(n // 10) + count_digit(n // 10, 10 - n % 10)
+
+
+def count_digit(n, digit):
+    if n == 0:
+        return 0
+    if n % 10 == digit:
+        return 1 + count_digit(n // 10, digit)
+    return count_digit(n // 10, digit)
