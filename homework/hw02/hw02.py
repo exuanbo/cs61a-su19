75a9beef816b77b787bd274bf0e56315c0885990
exuanbo <exuanbo@protonmail.com>
2021-03-03 17:12:48 +0000

    done with hw02

---
+++ homework/hw02/hw02.py
@@ -0,0 +1,327 @@
+""" Homework 2: Higher Order Functions"""
+
+HW_SOURCE_FILE = "hw02.py"
+
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+######################
+# Required Questions #
+######################
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+    n    -- a positive integer
+    term -- a function that takes one argument
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'product', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    while n >= 1:
+        result *= term(n)
+        n -= 1
+    return result
+
+
+def factorial(n):
+    """Return n factorial for n >= 0 by calling product.
+
+    >>> factorial(4)  # 4 * 3 * 2 * 1
+    24
+    >>> factorial(6)  # 6 * 5 * 4 * 3 * 2 * 1
+    720
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'factorial', ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return product(n, identity)
+
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument commutative, associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return base
+
+    def repeat_accumulate(i):
+        if i == 1:
+            return term(1)
+        return combiner(repeat_accumulate(i - 1), term(i))
+
+    return combiner(base, repeat_accumulate(n))
+
+
+def summation_using_accumulate(n, term):
+    """Returns the sum of term(1) + ... + term(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return accumulate(add, 0, n, term)
+
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return accumulate(mul, 1, n, term)
+
+
+def compose1(f, g):
+    """Return a function h, such that h(x) = f(g(x))."""
+
+    def h(x):
+        return f(g(x))
+
+    return h
+
+
+def make_repeater(f, n):
+    """Return the function that computes the nth application of f.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    # if n == 0:
+    #     return lambda x: x
+    # return lambda x: compose1(f, make_repeater(f, n - 1))(x)
+
+    return lambda x: accumulate(compose1, identity, n, lambda _: f)(x)
+
+
+def num_sevens(n):
+    """Returns the number of times 7 appears as a digit of n.
+
+    >>> num_sevens(3)
+    0
+    >>> num_sevens(7)
+    1
+    >>> num_sevens(7777777)
+    7
+    >>> num_sevens(2637)
+    1
+    >>> num_sevens(76370)
+    2
+    >>> num_sevens(12345)
+    0
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'num_sevens',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 0
+    if n % 10 == 7:
+        return 1 + num_sevens(n // 10)
+    return num_sevens(n // 10)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def pingpong(element, index, addend):
+        if index == n:
+            return element
+        if index % 7 == 0 or num_sevens(index) > 0:
+            return pingpong(element + (-addend), index + 1, -addend)
+        return pingpong(element + addend, index + 1, addend)
+
+    return pingpong(1, 1, 1)
+
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def get_max_denomination_starts_from(denomination):
+        assert (
+            denomination == 1
+            or (denomination != 0)
+            and (denomination & (denomination - 1) == 0)
+        )
+        if denomination > amount:
+            return denomination // 2
+        return get_max_denomination_starts_from(denomination * 2)
+
+    def count_change_partition(total_amount, max_denomination):
+        if total_amount == 0:
+            return 1
+        if total_amount < 0:
+            return 0
+        if max_denomination == 0:
+            return 0
+        return count_change_partition(
+            total_amount - max_denomination, max_denomination
+        ) + count_change_partition(total_amount, max_denomination // 2)
+
+    return count_change_partition(amount, get_max_denomination_starts_from(1))
+
+
+###################
+# Extra Questions #
+###################
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return print_move(start, end)
+    move_stack(n - 1, start, 6 - start - end)
+    move_stack(1, start, end)
+    move_stack(n - 1, 6 - start - end, end)
+
+
+from operator import sub, mul
+
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return (lambda f: f(f))(lambda f: lambda n: 1 if n == 0 else n * f(f)(n - 1))
