3c55bd6bd4f7fe1079dde64958d2b747ad224346
exuanbo <exuanbo@protonmail.com>
2021-03-04 21:04:39 +0000

    done with lab04

---
+++ labs/lab04/lab04.py
@@ -0,0 +1,158 @@
+""" Lab 04: Lists and Data Abstraction """
+
+
+def couple(s1, s2):
+    """Return a list that contains lists with i-th elements of two sequences
+    coupled together.
+    >>> s1 = [1, 2, 3]
+    >>> s2 = [4, 5, 6]
+    >>> couple(s1, s2)
+    [[1, 4], [2, 5], [3, 6]]
+    >>> s3 = ['c', 6]
+    >>> s4 = ['s', '1']
+    >>> couple(s3, s4)
+    [['c', 's'], [6, '1']]
+    """
+    assert len(s1) == len(s2)
+    "*** YOUR CODE HERE ***"
+    return [[s1[i], s2[i]] for i in range(len(s1))]
+
+
+def enumerate(s, start=0):
+    """Returns a list of lists, where the i-th list contains i+start and
+    the i-th element of s.
+    >>> enumerate([6, 1, 'a'])
+    [[0, 6], [1, 1], [2, 'a']]
+    >>> enumerate('five', 5)
+    [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
+    """
+    "*** YOUR CODE HERE ***"
+    return [[i, s[i - start]] for i in range(start, start + len(s))]
+
+
+from math import sqrt
+
+
+def distance(city1, city2):
+    """
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    """
+    "*** YOUR CODE HERE ***"
+    add_square = lambda a, b: a ** 2 + b ** 2
+    sub_with = lambda f: lambda a, b: f(a) - f(b)
+    sub_lat = sub_with(get_lat)
+    sub_lon = sub_with(get_lon)
+    return sqrt(add_square(sub_lat(city1, city2), sub_lon(city1, city2)))
+
+
+def closer_city(lat, lon, city1, city2):
+    """
+    Returns the name of either city1 or city2, whichever is closest to
+    coordinate (lat, lon).
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    "*** YOUR CODE HERE ***"
+    return get_name(
+        min(city1, city2, key=lambda city: distance(make_city(None, lat, lon), city))
+    )
+
+
+def check_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    >>> change_abstraction(False)
+    """
+
+
+# Treat all the following code as being behind an abstraction layer, you shouldn't need to look at it!
+
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name": name, "lat": lat, "lon": lon}
+    else:
+        return [name, lat, lon]
+
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
---
+++ labs/lab04/lab04_extra.py
@@ -0,0 +1,30 @@
+""" Lab 04 Optional Questions """
+
+from lab04 import *
+
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    return [int(sqrt(n)) for n in s if sqrt(n) % 1 == 0]
+
+
+def key_of_min_value(d):
+    """Returns the key in a dict d that corresponds to the minimum value of d.
+    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
+    >>> min(letters)
+    'a'
+    >>> key_of_min_value(letters)
+    'c'
+    """
+    "*** YOUR CODE HERE ***"
+    return min(d.keys(), key=lambda k: d[k])
