e5e1b662f59191c53e663805fcb6eb50ffd8bed7
exuanbo <exuanbo@protonmail.com>
2021-03-05 10:34:42 +0000

    init lab05

---
+++ labs/lab05/lab05.py
@@ -0,0 +1,131 @@
+LAB_SOURCE_FILE = "lab05.py"
+
+""" Lab 05: Trees and Proj2 Prep """
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+    >>> merge([1], [2])
+    [1, 2]
+    >>> merge([2], [1])
+    [1, 2]
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    if not lst1 or not lst2:
+        return []
+    elif lst1[0] < lst2[0]:
+        return [lst1[0]] + merge(lst1[1:], lst2)
+    else:
+        return [lst2[0]] + merge(lst1, lst2[1:])
+
+def add_chars(w1, w2):
+    """
+    Return a string containing the characters you need to add to w1 to get w2.
+
+    You may assume that w1 is a subsequence of w2.
+
+    >>> add_chars("owl", "howl")
+    'h'
+    >>> add_chars("want", "wanton")
+    'on'
+    >>> add_chars("rat", "radiate")
+    'diae'
+    >>> add_chars("a", "prepare")
+    'prepre'
+    >>> add_chars("resin", "recursion")
+    'curo'
+    >>> add_chars("fin", "effusion")
+    'efuso'
+    >>> add_chars("coy", "cacophony")
+    'acphon'
+    >>> from construct_check import check
+    >>> check(LAB_SOURCE_FILE, 'add_chars',
+    ...       ['For', 'While', 'Set', 'SetComp']) # Must use recursion
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+def acorn_finder(t):
+    """Returns True if t contains a node with the value 'acorn' and
+    False otherwise.
+
+    >>> scrat = tree('acorn')
+    >>> acorn_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('acorn')]), tree('branch2')])
+    >>> acorn_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> acorn_finder(numbers)
+    False
+    >>> t = tree(1, [tree('acorn',[tree('not acorn')])])
+    >>> acorn_finder(t)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+# Tree ADT
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
