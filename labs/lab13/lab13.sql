f3df9fb15640a3d02dabad90408cee3f26064d07
exuanbo <exuanbo@protonmail.com>
2021-03-11 23:06:39 +0000

    done with lab13

--- labs/lab13/lab13.sql
+++ labs/lab13/lab13.sql
@@ -1,13 +1,27 @@
 .read su19data.sql
 
 CREATE TABLE obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor
+  FROM students;
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest
+  FROM students
+  WHERE smallest > 2
+  ORDER BY smallest
+  LIMIT 20;
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students AS a, students AS b
+  WHERE
+    a.time < b.time AND
+    a.pet = b.pet AND
+    a.song = b.song;
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest
+  FROM students
+  GROUP BY smallest
+  HAVING COUNT(*) = 1
+  ORDER BY smallest;
--- labs/lab13/lab13_extra.sql
+++ labs/lab13/lab13_extra.sql
@@ -1,12 +1,22 @@
 .read lab13.sql
 
 CREATE TABLE su19favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*)
+  FROM students
+  GROUP BY pet
+  HAVING COUNT(*) > 1
+  ORDER BY COUNT(*) DESC, pet
+  LIMIT 10;
 
 
 CREATE TABLE su19dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*)
+  FROM students
+  WHERE pet = 'dog';
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor, COUNT(*)
+  FROM students
+  WHERE seven = '7'
+  GROUP BY instructor;
