8e940d3d058de6f797adbcfaad4aac48d9e1d5e1
exuanbo <exuanbo@protonmail.com>
2021-03-12 09:52:43 +0000

    done with hw07

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -4,9 +4,22 @@
     	(cons-stream (f (car s)) (map-stream f (cdr-stream s)))))
 
 (define multiples-of-three
-  'YOUR-CODE-HERE
-)
+  ; 'YOUR-CODE-HERE
+  (cons-stream 3 (map-stream (lambda (x) (+ x 3)) multiples-of-three)))
 
+; https://github.com/sgalal/cs61a/blob/master/Homework/hw07/hw07.scm#L9
 (define (rle s)
-  'YOUR-CODE-HERE
-)
+  ; 'YOUR-CODE-HERE
+  (define (insert-or-incr x xs)
+    (cond
+      ((null? xs) (cons-stream (list x 1) nil))
+      ((equal? x (car (car xs)))
+        (cons-stream
+          (list (car (car xs)) (+ 1 (car (cdr (car xs)))))
+          (cdr-stream xs)))
+      (else (cons-stream (car xs) (insert-or-incr x (cdr-stream xs))))))
+  (define (helper xs acc)
+    (if (null? xs)
+      acc
+      (helper (cdr-stream xs) (insert-or-incr (car xs) acc))))
+  (helper s nil))
--- homework/hw07/hw07.sql
+++ homework/hw07/hw07.sql
@@ -29,20 +29,56 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name, size
+  FROM dogs, sizes
+  WHERE min < height AND height <= max;
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dog_child.name
+  FROM
+    dogs AS dog_child,
+    parents,
+    dogs AS dog_parent
+  WHERE dog_child.name = child
+    AND dog_parent.name = parent
+  ORDER BY dog_parent.height DESC;
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT
+    dog_child_1.name as dog_1_name,
+    dog_child_2.name as dog_2_name
+  FROM
+    dogs as dog_child_1,
+    dogs AS dog_child_2,
+    parents AS dog_parents_1,
+    parents AS dog_parents_2
+  WHERE dog_1_name < dog_2_name
+    AND dog_1_name = dog_parents_1.child
+    AND dog_2_name = dog_parents_2.child
+    AND dog_parents_1.parent = dog_parents_2.parent;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT PRINTF("%s and %s are %s siblings", dog_1_name, dog_2_name, dog_sizes_1.size)
+  FROM
+    siblings,
+    dogs AS dogs_1,
+    dogs AS dogs_2,
+    sizes AS dog_sizes_1,
+    sizes AS dog_sizes_2
+  WHERE dogs_1.name = dog_1_name
+    AND dogs_2.name = dog_2_name
+    AND dog_sizes_1.min < dogs_1.height
+    AND dogs_1.height <= dog_sizes_1.max
+    AND dog_sizes_2.min < dogs_2.height
+    AND dogs_2.height <= dog_sizes_2.max
+    AND dog_sizes_1.size = dog_sizes_2.size;
 
 -- Total size for each fur type where all of the heights differ by no more than 30% from the average height
 CREATE TABLE low_variance AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT fur, SUM(height)
+  FROM dogs
+  GROUP BY fur
+  HAVING 0.7 * AVG(height) <= height AND height <= 1.3 * AVG(height);
